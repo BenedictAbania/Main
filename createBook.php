d2ad2cb9c7ef8a998bc1634e0b75a45a8f28996e
BenedictAbania <abaniachristian@gmail.com>
2025-09-29 00:21:40 +0800

    Update createBook.php

--- createBook.php
+++ createBook.php
@@ -37,7 +37,7 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Add New Book</title>
-    <link rel="stylesheet" href="assets/functions.css">
+    <link rel="stylesheet" href="functions.css">
 </head>
 
 <body>
